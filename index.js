a25e0875333743307f6cb979d46de635e52e77e1
arnold-oghiator <arnoldoghiator@gmail.com>
2022-07-23 16:04:44 +0100

    api implementation

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
- var stringArr = "arnold"
+const api =  {
+    key:'19e3572f962d0b559ca4cbbbfdac548c',
+    base:"https://api.openweathermap.org/data/2.5/"
+}
 
- console.log(stringArr)
+
+
+
+const Search  =document.querySelector('.search')
+const Btn  =document.querySelector('.btn')
+
+
+Btn.addEventListener('click', getInput)
+
+
+
+function getInput(e){
+    e.preventDefault()
+    if(event.click ==="click"){
+        getData(Search.value)
+    }
+}
