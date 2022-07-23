fc224e8103be6c55297b2fe598b8dd7a9c9fa992
arnold-oghiator <arnoldoghiator@gmail.com>
2022-07-23 21:11:42 +0100

    api integration

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const api =  {
 
 
 
-const Search  =document.querySelector('.search')
-const Btn  =document.querySelector('.btn')
+const Search = document.querySelector('.search')
+const Btn = document.querySelector('.btn')
+const ErrorC = document.querySelector('.error')
 
 
 Btn.addEventListener('click', getInput)
@@ -16,7 +17,26 @@ Btn.addEventListener('click', getInput)
 
 function getInput(e){
     e.preventDefault()
-    if(event.click ==="click"){
+    if(e.type == "click"){
+        ErrorC.textContent=""
         getData(Search.value)
+
+       
     }
 }
+
+async function getData(){
+  const data =  await fetch(`${api.base}weather?q=${Search.value}&units=metric&appid=${api.key}`)
+     const response = await data.json()
+     console.log(response)
+    if(response.cod == "404"){
+       ErrorC.textContent="please enter a valid city"
+       
+        Search.value = ""
+    }
+
+ 
+
+}
+
+
